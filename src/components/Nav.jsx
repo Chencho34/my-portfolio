afd092c036f2ea2cbf87e34cb5805bf527340b70
Armando <armandocrescencio343@gmail.com>
2023-04-20 21:56:51 -0600

    ADD: Interactive navigation links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { paths } from './constans'
 import { Link } from 'react-router-dom'
 
 export default function Nav() {
+  const [activeBtn, setActiveBtn] = useState(0)
+
+  const handleClick = (index) => {
+    setActiveBtn(index)
+  }
+
   return (
     <nav className='nav'>
       <section className='nav__container'>
         <h1 className='nav__title'>Chencho</h1>
         <ul className='nav__links'>
-          {paths.map(({ item, to }) => (
-            <li className='nav__link' key={item}>
+          {paths.map(({ item, to }, index) => (
+            <li
+              // className='nav__link'
+              key={item}
+              onClick={() => handleClick(index)}
+              className={
+                activeBtn === index ? 'nav__link--active' : 'nav__link'
+              }
+            >
               <Link to={to}>{item}</Link>
             </li>
           ))}
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function NotFound() {
   return (
     <section className='container'>
-      <h1>No found</h1>
+      <section className='not-found__contain'>
+        <h4 className='not-found__error'>404</h4>
+        <p className='not-found__text'>Not Found</p>
+      </section>
     </section>
   )
 }
